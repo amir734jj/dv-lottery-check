94454d36ab3d8d3034e87b4d61c01cc4ef8106d9
Seyedamirhossein Hesamian <amirhesamyan@gmail.com>
2023-05-07 22:15:10 -0500

    working state

---
+++ main.py
@@ -0,0 +1,189 @@
+import base64
+import datetime
+import io
+import time
+from threading import Thread
+
+from flask import Flask, render_template, request, url_for, redirect, send_file
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.wait import WebDriverWait
+from sqlalchemy import create_engine, Boolean, Column, String, Text, Integer, DateTime
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+SELENIUM_WAIT_SECONDS = 10
+
+# Selenium CSS selectors
+confirmation_elem = (By.CSS_SELECTOR, "#txtCN")
+lastname_elem = (By.CSS_SELECTOR, "#txtLastName")
+year_elem = (By.CSS_SELECTOR, "#txtYOB")
+captcha = (By.CSS_SELECTOR, "#txtCodeInput")
+captcha_image = (By.CSS_SELECTOR, "#c_checkstatus_uccaptcha30_CaptchaImage")
+submit = (By.CSS_SELECTOR, "#btnCSubmit")
+check_status_elem = (By.CSS_SELECTOR, "#maincontent > div:nth-child(3) > div > div > div.panel-body.p5555 > "
+                                      "div.col-xs-12.col-sm-12.text-center > div > p:nth-child(2) > a")
+continue_elem = (By.CSS_SELECTOR, "#main > div:nth-child(2) > div > p.text-center > a")
+
+# Flask app
+app = Flask(__name__, template_folder="templates")
+app.config['SQLALCHEMY_DATABASE_URI'] = ''
+
+engine = create_engine('sqlite:///db.sqlite')
+
+Base = declarative_base()
+Session = sessionmaker(bind=engine)
+
+
+def wait_until(some_predicate, timeout, period=0.25, *args, **kwargs):
+    must_end = datetime.datetime.now() + timeout
+    while datetime.datetime.now() < must_end:
+        if some_predicate(*args, **kwargs):
+            return True
+        time.sleep(period)
+    return False
+
+
+def clean_captcha(user_id):
+    with Session() as session:
+        session.query(User). \
+            update({"user_id": user_id,
+                    'captcha_image': '',
+                    "captcha_result": '',
+                    "check_result": False})
+        session.commit()
+
+
+def check_user_property_is_set(user_id, prop):
+    with Session() as session:
+        user = session.get(User, user_id)
+        return bool(getattr(user, prop))
+
+
+def check(user_id):
+    with Session() as session:
+        user = session.get(User, user_id)
+
+        driver = webdriver.Chrome()
+        try:
+            driver.get("https://dvprogram.state.gov/")
+
+            WebDriverWait(driver, SELENIUM_WAIT_SECONDS) \
+                .until(expected_conditions.presence_of_element_located(check_status_elem))
+            elem = driver.find_element(*check_status_elem)
+            elem.click()
+
+            WebDriverWait(driver, SELENIUM_WAIT_SECONDS) \
+                .until(expected_conditions.presence_of_element_located(continue_elem))
+            elem = driver.find_element(*continue_elem)
+            elem.click()
+
+            WebDriverWait(driver, SELENIUM_WAIT_SECONDS) \
+                .until(expected_conditions.presence_of_element_located(lastname_elem))
+
+            driver.find_element(*confirmation_elem).send_keys(user.confirmation_number)
+            driver.find_element(*lastname_elem).send_keys(user.lastname)
+            driver.find_element(*year_elem).send_keys(user.birth_year)
+
+            session.query(User). \
+                update({"user_id": user_id, 'captcha_image': driver.find_element(*captcha_image).screenshot_as_base64})
+            session.commit()
+
+            if not wait_until(lambda: check_user_property_is_set(user_id, "captcha_result"),
+                              datetime.timedelta(seconds=60)):
+                return False
+
+            session.refresh(user)
+            driver.find_element(*captcha).send_keys(user.captcha_result)
+            driver.find_element(*submit).click()
+            screenshot = driver.get_screenshot_as_base64()
+
+            session.query(User). \
+                update({"user_id": user_id,
+                        "check_result": True,
+                        "screenshot": screenshot,
+                        "last_update": datetime.datetime.utcnow()})
+            session.commit()
+        finally:
+            clean_captcha(user_id)
+            driver.close()
+
+
+class User(Base):
+    __tablename__ = 'users'
+    user_id = Column(Integer, primary_key=True)
+    lastname = Column(String(100))
+    confirmation_number = Column(String(100))
+    birth_year = Column(String(100))
+    captcha_image = Column(Text())
+    captcha_result = Column(String(100))
+    check_result = Column(Boolean())
+    last_update = Column(DateTime(), default=None)
+    screenshot = Column(Text())
+
+
+@app.route('/')
+def index():
+    with Session() as session:
+        return render_template('index.html.jinja', users=session.query(User).all())
+
+
+@app.route('/check/<user_id>', methods=['GET', 'POST'])
+def check_captcha(user_id):
+    with Session() as session:
+        user = session.get(User, user_id)
+        if request.method == 'GET':
+            clean_captcha(user_id)
+
+            t = Thread(target=check, args=[user_id])
+            t.start()
+
+            if not wait_until(lambda: check_user_property_is_set(user_id, "captcha_image"),
+                              datetime.timedelta(seconds=15)):
+                return "Record not found", 400
+
+            session.refresh(user)
+            return render_template('captcha.html.jinja', user=user)
+        else:
+            captcha_result = request.form.get('captcha')
+            session.query(User). \
+                update({"user_id": user_id, 'captcha_result': captcha_result})
+            session.commit()
+
+            if not wait_until(lambda: check_user_property_is_set(user_id, "check_result"),
+                              datetime.timedelta(seconds=5)):
+                return "Record not found", 400
+
+            session.refresh(user)
+            return render_template('check.html.jinja', user=user)
+
+
+@app.route('/user/create', methods=['GET', 'POST'])
+def create_user():
+    if request.method == 'GET':
+        return render_template('user.html.jinja')
+    else:
+        user = User()
+        user.lastname = request.form.get('lastname')
+        user.birth_year = request.form.get('birth_year')
+        user.confirmation_number = request.form.get('confirmation_number')
+
+        with Session() as session:
+            session.add(user)
+            session.commit()
+            return redirect(url_for('index'))
+
+
+@app.route('/user/screenshot/<user_id>', methods=['GET'])
+def user_screenshot(user_id):
+    with Session() as session:
+        return send_file(
+            io.BytesIO(base64.b64decode(session.get(User, user_id).screenshot)),
+            download_name='screenshot.png',
+            mimetype='image/png'
+        )
+
+
+with app.app_context():
+    Base.metadata.create_all(bind=engine)
+    app.run()
